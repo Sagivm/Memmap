936816c44d0c331271c9d601032c158b9fefc2a0
Sagivm <Sgmelamed@gmail.com>
2021-02-02 17:46:27 +0200

    Initial Gridd

--- src/index.js
+++ src/index.js
@@ -4,14 +4,56 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const MEM_LENGTH=20
+class Block extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      type: null,
+      filling_percent: this.props.size / 512 * 100
+    }
+  }
+  render(){
+    return(
+      <button class="square mem-block-25" 
+              onClick={()=>alert("click"
+              )}>
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class BoardColumn extends React.Component{
+  render(){
+    const items = []
+    for (var i=0; i<MEM_LENGTH; i++){
+      items.push(<Block value={i} />)
+    }
+    return(
+      <div className="board-row">
+        {items}
+      </div>
+    );
+  }
+}
+class MemmoryMap extends React.Component {
+  render() {
+    const items = []
+    for (var i=0; i<MEM_LENGTH; i++){
+      items.push(<BoardColumn />)
+    }
+    return (
+      <div>
+        {items}
+      </div>
+    )
+  }
+}
+
+// ========================================
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MemmoryMap />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
