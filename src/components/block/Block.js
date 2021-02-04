740cb2f2ca6d12f3b7133d41509caeea48452682
Sagivm <Sgmelamed@gmail.com>
2021-02-04 18:03:21 +0200

    hover

--- src/components/block/Block.js
+++ src/components/block/Block.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactTooltip from 'react-tooltip';
 import './Block.css';
+
+const MEM_BLOCK_SIZE=512
 
 class Block extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      index:this.props.index,
       type: null,
-      filling_percent: this.props.size / 512 * 100
+      start:this.props.start,
+      end:this.props.end,
+      filling_percent: this.props.size / MEM_BLOCK_SIZE * 100
     }
   }
   get_color(){
+    //TODO: maybe make this more elegent
     switch(Math.floor(this.state.filling_percent/25)*25){
 
       case(0):
@@ -33,11 +40,11 @@ class Block extends React.Component {
   render(){
     var color = this.get_color()
     return(
-      <button className={'square ' + color}
-        
-              onClick={()=>alert("click")}>
-        
-      </button>
+      <div>
+        <button type="button"
+                className={'square ' + color}
+                onClick={()=>alert("click")}/>    
+      </div>
     );
   }
 }
--- src/components/segment/Segment.js
+++ src/components/segment/Segment.js
@@ -5,13 +5,20 @@ import './Segment.css';
 import Block from '../block/Block'; 
 
 const MEM_LENGTH = 20;
+const MEM_BLOCK_SIZE=512;
 
 class BoardColumn extends React.Component{
     render(){
       const items = []
       for (var i=0; i<MEM_LENGTH; i++){
         items.push(
-           <Block size={260}/>
+           <Block 
+              size={260}
+              index={this.props.row*MEM_LENGTH + i}
+              start={(this.props.row*MEM_LENGTH + i)*MEM_BLOCK_SIZE} 
+              end={(this.props.row*MEM_LENGTH + i + 1)*MEM_BLOCK_SIZE - 1}
+
+              />
         )
       }
       return(
@@ -31,7 +38,7 @@ class BoardColumn extends React.Component{
     render() {
       const items = []
       for (var i=0; i<MEM_LENGTH; i++){
-        items.push(<BoardColumn />)
+        items.push(<BoardColumn row={i} />)
       }
       return (
         <div class="segment">
