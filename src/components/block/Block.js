50340198c6c0b5fcd7a376183338785d2c1160cc
Sagivm <Sgmelamed@gmail.com>
2021-02-04 19:37:43 +0200

    add basic tooltip

--- src/components/block/Block.js
+++ src/components/block/Block.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactTooltip from 'react-tooltip';
+import { format } from 'react-string-format';
 import './Block.css';
 
 const MEM_BLOCK_SIZE=512
+
+
 
 class Block extends React.Component {
   constructor(props){
@@ -13,11 +16,13 @@ class Block extends React.Component {
       type: null,
       start:this.props.start,
       end:this.props.end,
+      free:MEM_BLOCK_SIZE - this.props.size,
+      //TODO Maybe change
       filling_percent: this.props.size / MEM_BLOCK_SIZE * 100
     }
   }
-  get_color(){
-    //TODO: maybe make this more elegent
+  get_filling_class(){
+    //TODO: maybe make this more elegent and add more divition  
     switch(Math.floor(this.state.filling_percent/25)*25){
 
       case(0):
@@ -37,15 +42,42 @@ class Block extends React.Component {
         break;
     }
   }
+  
   render(){
-    var color = this.get_color()
-    return(
-      <div>
-        <button type="button"
-                className={'square ' + color}
-                onClick={()=>alert("click")}/>    
-      </div>
-    );
+    var filling_class = this.get_filling_class()
+    var size =30
+    var content = format('<h6>start: {0}</h6><h6>end: {1}</h6><h6>free: {2}</h6></div>',
+      this.state.start.toString(16),
+      this.state.end.toString(16),
+      (MEM_BLOCK_SIZE - this.props.size).toString(16));
+    if (this.props.selected)
+    {
+      return(
+        <div>
+          <button type="button"
+                  className={'square ' + filling_class + ' ' + (this.props.selected ? "selected": null) }
+                  onClick={()=>alert("click")}
+                  data-tip={content}/>
+          <ReactTooltip 
+              place="bottom"
+              type="light" 
+              effect="solid"
+              multiline
+              html={true}
+              />    
+        </div>
+      );
+
+    }
+    else{
+      return(
+        <div>
+          <button type="button"
+                  className={'square ' + filling_class + ' ' + (this.props.selected ? "selected": null) }
+                  onClick={()=>alert("click")}/>    
+        </div>
+      );
+    }
   }
 }
 
--- src/components/segment/Segment.js
+++ src/components/segment/Segment.js
@@ -7,19 +7,27 @@ import Block from '../block/Block';
 const MEM_LENGTH = 20;
 const MEM_BLOCK_SIZE=512;
 
+
+const TEST_INDEX = 42;
+
 class BoardColumn extends React.Component{
     render(){
+      //TODO move to pre render function
       const items = []
+      var selected = false;
       for (var i=0; i<MEM_LENGTH; i++){
+        if (this.props.row*MEM_LENGTH +i == TEST_INDEX)
+          selected = true;
         items.push(
            <Block 
-              size={260}
+              size={500}
               index={this.props.row*MEM_LENGTH + i}
               start={(this.props.row*MEM_LENGTH + i)*MEM_BLOCK_SIZE} 
               end={(this.props.row*MEM_LENGTH + i + 1)*MEM_BLOCK_SIZE - 1}
-
+              selected = {selected}
               />
         )
+        selected = false;
       }
       return(
         <div className="mem-row">
