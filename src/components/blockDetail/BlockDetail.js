6f34437fd04bf7dac694213379a37ff15fc7860f
Sagivm <Sgmelamed@gmail.com>
2021-02-06 13:53:59 +0200

    Move Board to BoardDetail

--- src/components/blockDetail/BlockDetail.js
+++ src/components/blockDetail/BlockDetail.js
@@ -7,11 +7,31 @@ class BlockDetail extends React.Component{
     constructor(props){
       super(props);
       this.state={
+          format:"Hex"
       }
     }
     
+    changeformat(){
+        
+    }
     render(){
-        return(null;)
+        return(
+            <div>
+                <div class="d-flex justify-content-center">
+                    <div className="col-sm">
+                        <h4>Start:</h4>
+                        <h4>Size: </h4>
+                    </div>
+                    <div className="col-sm">
+                        <h4>End:</h4>
+                        <h4>Free:</h4>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button type="button" class="btn btn-primary" onClick={()=> this.changeformat()}>Hex</button>
+                </div>
+            </div>
+        );
     }
 }
 export default BlockDetail;
