1fd91439da0a15664361edf474d033096e02b72d
Sagivm <Sgmelamed@gmail.com>
2021-02-06 13:48:09 +0200

    Add blockDetail

---
+++ src/components/blockDetail/BlockDetail.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './BlockDetail.css';
+
+class BlockDetail extends React.Component{
+    
+    constructor(props){
+      super(props);
+      this.state={
+      }
+    }
+    
+    render(){
+        return(null;)
+    }
+}
+export default BlockDetail;
