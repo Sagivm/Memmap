59616e121274255dab2d7ce243dc74211257f453
Sagivm <Sgmelamed@gmail.com>
2021-02-13 18:31:38 +0200

    add options for search

---
+++ src/components/search/Search-fetch.js
@@ -0,0 +1,46 @@
+export const units = [
+    { value: '1', label: 'd_pro_1', color: '#00B8D9'},
+    { value: '2', label: 'd_pro_2', color: '#00B8D9'},
+    { value: '3', label: 'd_pro_3', color: '#00B8D9'},
+    { value: '4', label: 'd_pro_4', color: '#00B8D9'},
+  ];
+export const regions = [
+    { value: '1', label: 'dgrp1data', color: '#00B8D9'},
+    { value: '2', label: 'dgrp2data', color: '#00B8D9'},
+    { value: '3', label: 'dgrp3data', color: '#00B8D9'},
+    { value: '4', label: 'dgrp4data', color: '#00B8D9'},
+  ];
+
+export const addressStates = [
+    { value: 'As-0', label: 'As-0', color: '#00B8D9'},
+    { value: 'As-1', label: 'As-1', color: '#00B8D9'},
+    { value: 'As-2', label: 'As-2', color: '#00B8D9'},
+    { value: 'As-3', label: 'As-3', color: '#00B8D9'},
+    { value: 'As-4', label: 'As-4', color: '#00B8D9'},
+    { value: 'As-5', label: 'As-5', color: '#00B8D9'},
+    { value: 'As-6', label: 'As-6', color: '#00B8D9'},
+    { value: 'As-7', label: 'As-7', color: '#00B8D9'},
+    { value: 'As-8', label: 'As-8', color: '#00B8D9'},
+    { value: 'As-9', label: 'As-9', color: '#00B8D9'},
+    { value: 'As-10', label: 'As-10', color: '#00B8D9'},
+    { value: 'As-11', label: 'As-11', color: '#00B8D9'},
+    { value: 'As-12', label: 'As-12', color: '#00B8D9'},
+    { value: 'As-13', label: 'As-13', color: '#00B8D9'},
+    { value: 'As-14', label: 'As-14', color: '#00B8D9'},
+    { value: 'As-15', label: 'As-15', color: '#00B8D9'},
+  ];
+
+export const groupedOptions = [
+    {
+      label: 'Units',
+      options: units,
+    },
+    {
+      label: 'Regions',
+      options: regions,
+    },
+    {
+      label: 'Address-States',
+      options: addressStates,
+    },
+  ];
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Select from 'react-select'
 import './Search.css';
+import {groupedOptions} from './Search-fetch';
 
 class Search extends React.Component {
   constructor(props){
@@ -10,18 +11,12 @@ class Search extends React.Component {
     }
   }
   render(){
-    const colourOptions = [
-      { value: '1', label: 'd_pro_1', color: '#00B8D9'},
-      { value: '2', label: 'd_pro_2', color: '#00B8D9'},
-      { value: '3', label: 'd_pro_3', color: '#00B8D9'},
-      { value: '4', label: 'd_pro_4', color: '#00B8D9'},
-    ];
 
       return(
         <Select
             isMulti
-            name="colors"
-            options={colourOptions}
+            name=""
+            options={groupedOptions}
             className="basic-multi-select"
             classNamePrefix="Search unit"
         />
